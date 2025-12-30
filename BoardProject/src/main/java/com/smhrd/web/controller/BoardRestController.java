1d2c94aea3d6ecb3fe93281d4a044e860e6387e7
Hyunsu <khs95470039@gmail.com>
2025-12-30 10:28:23 +0900

    board detail, downlaod fuc

--- BoardProject/src/main/java/com/smhrd/web/controller/BoardRestController.java
+++ BoardProject/src/main/java/com/smhrd/web/controller/BoardRestController.java
@@ -3,18 +3,24 @@ package com.smhrd.web.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.smhrd.web.dto.BoardRequestDTO;
 import com.smhrd.web.entity.Board;
 import com.smhrd.web.service.BoardService;
 
 @RestController
 @RequestMapping("/api/board")
-//@CrossOrigin(origins= "http://127.0.0.1:5500", allowedHeaders = "*")
-@CrossOrigin(origins= "http://10.1.1.6", allowedHeaders = "*")
+@CrossOrigin(origins= "http://127.0.0.1:5500", allowedHeaders = "*")
+//@CrossOrigin(origins= "http://10.1.1.6", allowedHeaders = "*")
 public class BoardRestController {
 	
 	@Autowired
@@ -25,4 +31,36 @@ public class BoardRestController {
 		
 		return service.getList();
 	}
+	
+	@PostMapping("/register")
+	public String register(@ModelAttribute BoardRequestDTO dto) {
+		try {
+			service.register(dto);
+			return "success";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "fail";
+		}
+		
+	}
+	
+	@GetMapping("/{idx}")
+	public Board getDetail(@PathVariable("idx") Long b_idx) {
+		System.out.println("요청한 특정 게시글 글번호:"+b_idx);
+		Board board = service.getDetail(b_idx);
+		System.out.println("조회된 게시글 데이터 :"+board);
+		return board;
+	}
+	
+	@GetMapping("/{idx}/download")
+	public ResponseEntity<Resource> download(@PathVariable("idx") Long idx) {
+		try {
+			return service.download(idx);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+	
 }
---
+++ BoardProject/src/main/java/com/smhrd/web/dto/BoardRequestDTO.java
@@ -0,0 +1,17 @@
+package com.smhrd.web.dto;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardRequestDTO {
+	private String b_title;
+	private String b_writer;
+	private String b_content;
+	private MultipartFile b_file;
+}
